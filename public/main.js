4daaed659934307786896742004c2f4d664d5008
Sergio Varela <varela.a.sergio@gmail.com>
2022-11-23 15:04:49 -0700

    added new base url

--- public/main.js
+++ public/main.js
@@ -3,3 +3,19 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+
+
+const btn2 = document.querySelector('#button2')
+
+const getCat = () => {
+    axios.get('http://ec2-34-212-123-158.us-west-2.compute.amazonaws.com/api/cat')
+        .then(res => {
+            alert(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+btn2.addEventListener('click', getCat)
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+
 app.use(express.static(`${__dirname}/public`))
+
+app.get('/api/cat', (req, res) => {
+    res.status(200).send('Macy')
+})
 
 app.listen(4000,
    () => console.log(`server running on 4000`)
 )
+
